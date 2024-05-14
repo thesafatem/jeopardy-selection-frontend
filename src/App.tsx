c7997010bc15dbc1ae5cda0354ce109d7f6054e2
thesafatem <thesafatem@gmail.com>
2024-05-14 16:45:48 +0500

    Add table and highlight top teams

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,14 @@ interface IUser {
 }
 
 function Input() {
+    const topPlayers = 2
     const [users, setUsers] = useState<IUser[]>([]);
     const [name, setName] = useState("")
     const [score, setScore] = useState(0)
     const handleSubmit = (event: any) => {
-        setUsers([...users, { name, score }])
+        const nextUsers = [...users, { name, score }]
+        nextUsers.sort((a, b) => b.score - a.score)
+        setUsers(nextUsers)
     }
 
     const handleNameChange = (event: any) => {
@@ -22,23 +25,47 @@ function Input() {
         setScore(event.target.value)
     }
 
+    const headers = [
+        {
+            name: '№',
+            className: 'number'
+        },
+        {
+            name: 'Name',
+            className: 'name'
+        },
+        {
+            name: 'Score',
+            className: 'score'
+        }
+    ]
+
     return (
       <div>
           <input type="text" value={name} onChange={handleNameChange}/>
           <input type="number" value={score} onChange={handleScoreChange}/>
           <button onClick={handleSubmit}>Submit</button>
-          <ul>
-              {users.map(user => <li>{user.name}: {user.score}</li>)}
-          </ul>
+          <table className="table">
+              <tr>
+                  {headers.map(header => <th className={header.className}>{header.name}</th>)}
+              </tr>
+              {users.map((user, id) => (
+                  <tr className={id < topPlayers ? 'top-row' : 'bottom-row'}>
+                      <td>{id + 1}</td>
+                      <td>{user.name}</td>
+                      <td>{user.score}</td>
+                  </tr>
+              ))}
+          </table>
       </div>
     )
 }
 
 function App() {
   return (
-      <div>
+      <>
         <Input/>
-      </div>
+      </>
   );
 }
 
